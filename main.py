fb4039171e23d5a5ac5cc39bcd9fd3ac02b18214
bit-zalel <55101808+bit-zalel@users.noreply.github.com>
2019-11-27 12:40:16 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,32 +1,32 @@
 import sys
 from statistics import mean
 
+def isFloat(string):
+    try:
+        float(string)
+        return True
+    except ValueError:
+        return False
+
 def main():
     assert len(sys.argv) > 1, "No input file path specified."
 
     input_file_path = sys.argv[1]
     print(f"Processing input file: {input_file_path}")
 
-  
-
+    
     f = open(input_file_path,"r")
-    values = value = f.read()
+    values = f.read()
     values = values.split("\n")
 
     for val in values:
         strvals = val.split(",")
         numvals = []
         for nv in strvals:
-            if nv.isnumeric():
+            if isFloat(nv):
                 numvals.append(float(nv))
         if numvals != []:
             print(mean(numvals))
-
-
-
-
-    value.split("\n")
-
 
 if __name__ == "__main__":
     main()
