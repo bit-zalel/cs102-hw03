6c010cee52da129cb6cc5ee428af4725935077f7
bit-zalel <55101808+bit-zalel@users.noreply.github.com>
2019-11-27 11:57:53 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import sys
-
+from statistics import mean
 
 def main():
     assert len(sys.argv) > 1, "No input file path specified."
@@ -7,7 +7,25 @@ def main():
     input_file_path = sys.argv[1]
     print(f"Processing input file: {input_file_path}")
 
-    # TODO: Fill in the actual logic here!
+  
+
+    f = open(input_file_path,"r")
+    values = value = f.read()
+    values = values.split("\n")
+
+    for val in values:
+        strvals = val.split(",")
+        numvals = []
+        for nv in strvals:
+            if nv.isnumeric():
+                numvals.append(float(nv))
+        if numvals != []:
+            print(mean(numvals))
+
+
+
+
+    value.split("\n")
 
 
 if __name__ == "__main__":
